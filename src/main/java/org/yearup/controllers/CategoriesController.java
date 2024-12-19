96f7771e29b144ec772c9fa06085dcfa7285419b
Taseem Molique <taseenm100@gmail.com>
2024-12-19 11:39:27 -0500

    phase 1 and phase 2 complete

---
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,99 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.List;
+
+// add the annotations to make this a REST controller
+@RestController
+@RequestMapping("categories")
+@CrossOrigin
+// add the annotation to make this controller the endpoint for the following url
+    // http://localhost:8080/categories
+// add annotation to allow cross site origin requests
+public class CategoriesController
+{
+    private CategoryDao categoryDao;
+    private ProductDao productDao;
+
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
+
+   @GetMapping("")
+    public List<Category> getAll()
+    {
+        // find and return all categories
+        return categoryDao.getAllCategories();
+    }
+
+    @GetMapping("{id}")
+    public Category getById(@PathVariable int id)
+    {
+        // get the category by id
+       Category category = categoryDao.getById(id);
+
+        if (category == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return category;
+    }
+
+    // the url to return all products in category 1 would look like this
+    // https://localhost:8080/categories/1/products
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        // get a list of product by categoryId
+        return productDao.listByCategoryId(categoryId);
+    }
+
+    // add annotation to call this method for a POST action
+    // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Category addCategory(@RequestBody Category category)
+    {
+        // insert the category
+        try
+        {
+            return categoryDao.create(category);
+        }
+        catch(Exception ex)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+
+    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("{id}")
+    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        categoryDao.update(id, category);
+    }
+
+
+    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable int id)
+    {
+        categoryDao.delete(id);
+    }
+}
---
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -0,0 +1,173 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.CategoryDao;
+import org.yearup.models.Category;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
+{
+    public MySqlCategoryDao(DataSource dataSource)
+    {
+        super(dataSource);
+    }
+
+    @Override
+    public List<Category> getAllCategories()
+    {
+        // get all categories
+        List<Category> categories = new ArrayList<>();
+        String sql = """
+    SELECT *
+    FROM categories;
+    """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement query = connection.prepareStatement(sql);
+             ResultSet resultSet = query.executeQuery()) {
+
+            while (resultSet.next()) {
+                categories.add(mapRow(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return categories;
+    }
+
+    @Override
+    public Category getById(int categoryId)
+    {
+        // get category by id
+
+        String sql = """
+                SELECT
+                *
+                FROM categories
+                WHERE category_id = ?;""";
+
+        try(Connection connection = getConnection();
+            PreparedStatement query = connection.prepareStatement(sql);
+        )
+        {
+            query.setInt(1, categoryId);
+            try( ResultSet resultSet = query.executeQuery()){
+                while (resultSet.next()){
+                    return mapRow(resultSet);
+                }
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Category create(Category category)
+    {
+        // create a new category
+        String sql = """
+    INSERT INTO categories (name, description)
+    VALUES (?, ?)
+    """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement query = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            query.setString(1, category.getName());
+            query.setString(2, category.getDescription());
+
+            query.executeUpdate();
+
+            try (ResultSet resultSet = query.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    category.setCategoryId(resultSet.getInt(1)); // Set the generated ID to the category
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return category;
+        }
+
+    @Override
+    public void update(int categoryId, Category category)
+    {
+        // update category
+        String sql = """
+                UPDATE categories
+                SET name = ?, description = ?
+                WHERE category_id = ?
+                """;
+
+        try(Connection connection = getConnection();
+            PreparedStatement query = connection.prepareStatement(sql);
+        )
+        {
+            query.setString(1, category.getName());
+            query.setString(2, category.getDescription());
+            query.setInt(3, categoryId);
+
+            query.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void delete(int categoryId)
+    {
+        // delete category
+        String sql = """
+                DELETE FROM categories
+                WHERE category_id = ?
+                """;
+
+        try(Connection connection = getConnection();
+            PreparedStatement query = connection.prepareStatement(sql);
+        )
+        {
+            query.setInt(1, categoryId);
+            query.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Category mapRow(ResultSet row) throws SQLException
+    {
+        int categoryId = row.getInt("category_id");
+        String name = row.getString("name");
+        String description = row.getString("description");
+
+        Category category = new Category()
+        {{
+            setCategoryId(categoryId);
+            setName(name);
+            setDescription(description);
+        }};
+
+        return category;
+    }
+
+}
